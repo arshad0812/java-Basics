7a088cb0da4efbb07bc4238add8916067dd89b4f
arshad8012 <adarshadaashish@gmail.com>
2025-04-03 23:14:53 +0530

    Commit on 3/4/25

--- variables.java
+++ variables.java
@@ -1,6 +1,10 @@
 public class variables{
+
+    static int InstanceVariable;
     public static void main(String args[]){
         
+
+        //Data Types
         int var1;
         var1=1;
         double var2 = 3.14;
@@ -8,7 +12,22 @@ public class variables{
         boolean var4 = true;
         String var5 = "Hi";
 
-
         System.out.println(var1+" "+var2+" "+var3+" "+var4+" "+var5);
+
+
+        //Variable Scopes
+        // Local Scope
+        int localVar = 10; // Local variable
+        System.out.println(localVar);
+
+        // Static variable
+        InstanceVariable=10;
+        System.out.println(InstanceVariable);
+
+        //Variable Types
+        //Final Unchangable
+        final double PI = 3.14159;
+        // PI=1.1;
+        // This gives error
     }
 }
